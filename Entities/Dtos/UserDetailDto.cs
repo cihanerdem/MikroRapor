f282583c01d8cc4d18aab5ed16d78b69ce05b0bc
Cihan Erdem <erdem_cihan@hotmail.com>
2022-03-31 13:11:18 +0300

    Arka Planda User Bilgisi göstermek ve kullanmak için Detail Dto oluşturuldu

---
+++ Entities/Dtos/UserDetailDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos
+{
+   public class UserDetailDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string  Department { get; set; }
+        public string GSM { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int UserRole { get; set; }
+        public string UseDatabeses { get; set; }
+
+    }
+}
