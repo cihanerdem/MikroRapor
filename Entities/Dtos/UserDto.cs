d9f1594a554822bd3b4303799d9a8799bcc269b6
Cihan Erdem <erdem_cihan@hotmail.com>
2022-03-31 13:11:46 +0300

    User Bilgisi ve Kaydı için Ayrı Bir Dto oluşturuldu

---
+++ Entities/Dtos/UserDto.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Abstract;
+using System;
+
+namespace Entities.Dtos
+{
+    public class UserDto:IDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Department { get; set; }
+        public string GSM { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int UserRole { get; set; }
+        public string UseDatabeses { get; set; }
+
+        public string Token { get; set; }
+        public DateTime? TokenExpireDate { get; set; }
+    }
+}
