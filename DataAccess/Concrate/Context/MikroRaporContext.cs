e3df4a95a5e0a8b18a649374fb1da15731eb2ed6
Cihan Erdem <erdem_cihan@hotmail.com>
2022-03-31 14:54:53 +0300

    Database bağlantısı için Context oluşturuldu

---
+++ DataAccess/Concrate/Context/MikroRaporContext.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.Concrate.EntityFrameWork.Mapping;
+using Entities.Concrate.BaseEntities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrate.Context
+{
+    public class MikroRaporContext:DbContext
+    {
+        public MikroRaporContext(DbContextOptions<MikroRaporContext> options):base(options)
+        {
+
+        }
+        public MikroRaporContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Data Source=DESKTOP-BP9BGS3;Initial Catalog=MikroReportDb;Integrated Security=True";
+            optionsBuilder.UseSqlServer(connectionString);
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
