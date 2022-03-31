df40d06850787678eb11ab15415793bd560dcff6
Cihan Erdem <erdem_cihan@hotmail.com>
2022-03-31 13:09:32 +0300

    Tablo yapısı için AuditableEntity oluşturuldu

---
+++ Entities/Concrate/BaseEntities/AuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrate.BaseEntities
+{
+    public class AuditableEntity:BaseEntity
+    {
+    }
+}
