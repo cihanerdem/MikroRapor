0a77a96f133db23d15f87cf667905727ba11807d
Cihan Erdem <erdem_cihan@hotmail.com>
2022-03-31 13:09:08 +0300

    IEntity ınterface oluşturuldu

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Abstract
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+        DateTime? CreateDate { get; set; }
+        int CreateUser { get; set; }
+        DateTime? UpdateDate { get; set; }
+        int UpdateUser { get; set; }
+
+    }
+}
