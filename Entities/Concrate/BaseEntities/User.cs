e9708d6a4296606cbbdcf30beed6f42fa84947d8
Cihan Erdem <erdem_cihan@hotmail.com>
2022-03-31 13:10:39 +0300

    User Tablosu için gerekli properties sınıfı olusturuldu

---
+++ Entities/Concrate/BaseEntities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities.Concrate.BaseEntities
+{
+    public class User:AuditableEntity
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+         public int  Department { get; set; }
+        public string GSM { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int UserRole { get; set; }
+        public string UseDatabeses { get; set; }
+
+        public string Token { get; set; }
+        public DateTime? TokenExpireDate { get; set; }
+      
+    }
+}
