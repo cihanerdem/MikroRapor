b0444736b5b1c86112dfb856aa1b58ab82649088
Cihan Erdem <erdem_cihan@hotmail.com>
2022-03-31 13:10:17 +0300

    Departman Tablo yapısı için class oluşturuldu

---
+++ Entities/Concrate/BaseEntities/Department.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Entities.Concrate.BaseEntities
+{
+    public class Department:AuditableEntity
+    {
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+       // public virtual ICollection<User>  Users { get; set; }
+
+
+    }
+}
