603c9e600e620c5786b412fe8f4db14ceeeca263
Cihan Erdem <erdem_cihan@hotmail.com>
2022-04-01 10:13:49 +0300

    User Interface eklendi

---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrate.BaseEntities;
+using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDetailDto>> GetListAsync();
+        Task<UserDto> GetByIdAsync(int id);
+       // Task<UserDto> GetAsync(Expression<Func<User, bool>> filter);
+        Task<UserDto> AddAsync(UserDto userAdd);
+        Task<UserDto> Updatesync(UserDto userUpdate);
+        Task<bool> DeleteAsync(int id);
+    }
+}
