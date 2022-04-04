068bfcd8107e8f7aae1153366556f4f1adc4f0d4
Cihan Erdem <erdem_cihan@hotmail.com>
2022-04-04 14:38:53 +0300

    Category Service Yazıldı

---
+++ Business/Abstract/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDetailDto>> GetListAsync();
+        Task<CategoryDto> GetByIdAsync(int id);
+        // Task<UserDto> GetAsync(Expression<Func<User, bool>> filter);
+        Task<CategoryDto> AddAsync(CategoryDto categoryAdd);
+        Task<CategoryDto> Updatesync(CategoryDto categoryUpdate);
+        Task<bool> DeleteAsync(int id);
+    }
+}
