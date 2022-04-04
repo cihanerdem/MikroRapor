11b9773ea3bb9bb527909912dfa352b4f2479652
Cihan Erdem <erdem_cihan@hotmail.com>
2022-04-04 10:39:29 +0300

    Menu Tablo sınıfı yapılandırıldı

---
+++ Entities/Concrate/BaseEntities/AppMenu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrate.BaseEntities
+{
+   public class AppMenu:AuditableEntity
+    {
+        public int MenuNo { get; set; }
+        public string StaffIdToAccess { get; set; }
+        public string MenuName { get; set; }
+        public string Description { get; set; }
+        public int AssociatedMenuId { get; set; }
+        public int CategoryId { get; set; }
+        public string Url { get; set; }
+    }
+}
