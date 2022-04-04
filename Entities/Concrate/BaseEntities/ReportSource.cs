9dde75f84f6c249979981574ca13bfa0f4b5a850
Cihan Erdem <erdem_cihan@hotmail.com>
2022-04-04 10:40:19 +0300

    ReportSource Tablo properties leri tanımlandı

---
+++ Entities/Concrate/BaseEntities/ReportSource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrate.BaseEntities
+{
+    public class ReportSource:AuditableEntity
+    {
+        public string ReportCode { get; set; }
+        public int ReportNumber { get; set; }
+        public int CategoryId { get; set; }
+        public string ReportName { get; set; }
+        public byte[] SourceCode { get; set; }
+        public int MenuId { get; set; }
+        public string UserToAccess { get; set; }
+
+
+    }
+}
