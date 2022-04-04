0298feddd641fb7e5661fc68430550e4f00f57c8
Cihan Erdem <erdem_cihan@hotmail.com>
2022-04-04 10:39:52 +0300

    Kategori Tablo propertileri tanımlandı

---
+++ Entities/Concrate/BaseEntities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrate.BaseEntities
+{
+    public class Category:AuditableEntity
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
