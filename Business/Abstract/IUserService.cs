54d3c98252f5362c97668c165f56d4e50ed4b4ca
Cihan Erdem <erdem_cihan@hotmail.com>
2022-04-04 14:38:18 +0300

    Kod Temizliği Yapıldı

--- Business/Abstract/IUserService.cs
+++ Business/Abstract/IUserService.cs
@@ -1,10 +1,5 @@
-﻿using Entities.Concrate.BaseEntities;
-using Entities.Dtos;
-using System;
+﻿using Entities.Dtos;
 using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Business.Abstract
@@ -13,7 +8,7 @@ namespace Business.Abstract
     {
         Task<IEnumerable<UserDetailDto>> GetListAsync();
         Task<UserDto> GetByIdAsync(int id);
-       // Task<UserDto> GetAsync(Expression<Func<User, bool>> filter);
+        // Task<UserDto> GetAsync(Expression<Func<User, bool>> filter);
         Task<UserDto> AddAsync(UserDto userAdd);
         Task<UserDto> Updatesync(UserDto userUpdate);
         Task<bool> DeleteAsync(int id);
