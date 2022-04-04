df849809d46ab368df27c2a8241637dc855c9950
Cihan Erdem <erdem_cihan@hotmail.com>
2022-04-04 14:38:35 +0300

    AppMenu Service Yazıldı

---
+++ Business/Abstract/IAppMenuService.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IAppMenuService
+    {
+        Task<IEnumerable<AppenuDetailDto>> GetListAsync();
+        Task<AppMenuDto> GetByIdAsync(int id);
+        // Task<UserDto> GetAsync(Expression<Func<User, bool>> filter);
+        Task<AppMenuDto> AddAsync(AppMenuDto menuAdd);
+        Task<AppMenuDto> Updatesync(AppMenuDto menuUpdate);
+        Task<bool> DeleteAsync(int id);
+    }
+}
