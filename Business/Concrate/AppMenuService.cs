f3707e5d91ebf8b7b3eb5c93c43aeedd0132f8f3
Cihan Erdem <erdem_cihan@hotmail.com>
2022-04-04 14:39:55 +0300

    AppMenu Servis Repository ve Category Service Repository yazıldı User Service Düzenlendi

---
+++ Business/Concrate/AppMenuService.cs
@@ -0,0 +1,132 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrate.BaseEntities;
+using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Business.Concrate
+{
+    public class AppMenuService : IAppMenuService
+    {
+        IAppMenuDal _appMenu;
+
+        public AppMenuService(IAppMenuDal appMenu)
+        {
+            _appMenu = appMenu;
+        }
+
+        public async Task<AppMenuDto> AddAsync(AppMenuDto menuAdd)
+        {
+            AppMenu app = new AppMenu
+            {
+                CreateUser = 1,
+                CreateDate = DateTime.Now,
+                MenuName = menuAdd.MenuName,
+                AssociatedMenuId = menuAdd.AssociatedMenuId,
+                CategoryId = menuAdd.CategoryId,
+                Description = menuAdd.Description,
+                MenuNo = menuAdd.MenuNo,
+                StaffIdToAccess = menuAdd.StaffIdToAccess,
+                Url = menuAdd.Url
+            };
+            var menu = await _appMenu.AddAsync(app);
+            AppMenuDto dto = new AppMenuDto
+            {
+                MenuNo = menu.MenuNo,
+                CategoryId = menu.CategoryId,
+                AssociatedMenuId = menu.AssociatedMenuId,
+                Description = menu.Description,
+                MenuName = menu.MenuName,
+                StaffIdToAccess = menu.StaffIdToAccess,
+                Url = menu.Url,
+                Id = menu.Id
+            };
+            return dto;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return await _appMenu.DeleteAsync(id);
+        }
+
+        public async Task<AppMenuDto> GetByIdAsync(int id)
+        {
+
+            var app = await _appMenu.GetAsync(x => x.Id == id);
+            AppMenuDto menuDto = new AppMenuDto
+            {
+                Id = app.Id,
+                MenuNo = app.MenuNo,
+                MenuName = app.MenuName,
+                CategoryId = app.CategoryId,
+                AssociatedMenuId = app.AssociatedMenuId,
+                Description = app.Description,
+                StaffIdToAccess = app.StaffIdToAccess,
+                Url = app.Url
+
+            };
+            return menuDto;
+        }
+
+        public async Task<IEnumerable<AppenuDetailDto>> GetListAsync()
+        {
+            List<AppenuDetailDto> menuList = new List<AppenuDetailDto>();
+            var response = await _appMenu.GetListAsync();
+            foreach (var item in response.ToList())
+            {
+                AppenuDetailDto menu = new AppenuDetailDto
+                {
+                    Id = item.Id,
+                    MenuNo = item.MenuNo,
+                    CategoryId = item.CategoryId.ToString(),
+                    Description = item.Description,
+                    AssociatedMenuId = item.AssociatedMenuId,
+                    MenuName = item.MenuName,
+                    StaffIdToAccess = item.StaffIdToAccess
+
+                };
+                menuList.Add(menu);
+            }
+            return menuList;
+        }
+
+        public async Task<AppMenuDto> Updatesync(AppMenuDto menuUpdate)
+        {
+            var getMenu = await _appMenu.GetAsync(x => x.Id == menuUpdate.Id);
+            AppMenu appMenu = new AppMenu
+            {
+                CreateDate = getMenu.CreateDate,
+                CreateUser = getMenu.CreateUser,
+                AssociatedMenuId = menuUpdate.AssociatedMenuId,
+                CategoryId = menuUpdate.CategoryId,
+                Description = menuUpdate.Description,
+                Id = menuUpdate.Id,
+                MenuName = menuUpdate.MenuName,
+                MenuNo = menuUpdate.MenuNo,
+                StaffIdToAccess = menuUpdate.StaffIdToAccess,
+                UpdateDate = DateTime.Now,
+                //To do: Update User Kısmı Kullanıcıya göre Değiştirilecek
+                UpdateUser = 1,
+                Url = menuUpdate.Url
+
+            };
+            var updateApp = await _appMenu.UpdateAsync(appMenu);
+            AppMenuDto appDto = new AppMenuDto
+            {
+                Id = updateApp.Id,
+                AssociatedMenuId = updateApp.AssociatedMenuId,
+                CategoryId = updateApp.CategoryId,
+                Description = updateApp.Description,
+                MenuName = updateApp.MenuName,
+                MenuNo = updateApp.MenuNo,
+                Url = updateApp.Url,
+                StaffIdToAccess = updateApp.StaffIdToAccess
+
+            };
+            return appDto;
+        }
+    }
+}
---
+++ Business/Concrate/CategoryService.cs
@@ -0,0 +1,114 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrate.BaseEntities;
+using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Business.Concrate
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryDal _category;
+
+        public CategoryService(ICategoryDal category)
+        {
+            _category = category;
+        }
+
+        public async Task<CategoryDto> AddAsync(CategoryDto categoryAdd)
+        {
+            Category app = new Category
+            {
+                CreateUser = 1,
+                CreateDate = DateTime.Now,
+                Code = categoryAdd.Code,
+                Description = categoryAdd.Description,
+                Id = 1,
+                Name = categoryAdd.Name,
+
+            };
+            var cat = await _category.AddAsync(app);
+            CategoryDto dto = new CategoryDto
+            {
+              Id = cat.Id,
+              Code=cat.Code,
+              Name=cat.Name,
+              Description=cat.Description
+            };
+            return dto;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return await _category.DeleteAsync(id);
+        }
+
+        public async Task<CategoryDto> GetByIdAsync(int id)
+        {
+            var getCat = await _category.GetAsync(x => x.Id == id);
+            CategoryDto catDto = new CategoryDto
+            {
+                Id = getCat.Id,
+               Code=getCat.Code,
+               Description=getCat.Description,
+               Name=getCat.Name
+
+            };
+            return catDto;
+        }
+
+        public async Task<IEnumerable<CategoryDetailDto>> GetListAsync()
+        {
+            List<CategoryDetailDto> catList = new List<CategoryDetailDto>();
+            var response = await _category.GetListAsync();
+            foreach (var item in response.ToList())
+            {
+                CategoryDetailDto cat = new CategoryDetailDto
+                {
+                    Id = item.Id,
+                    Code=item.Code,
+                    CreateDate=(DateTime)item.CreateDate,
+                    CreateUser=item.CreateUser,
+                    Name=item.Name,
+                    Description = item.Description,
+                    UpdateDate=item.UpdateDate,
+                    UpdateUser=item.UpdateUser
+
+                };
+                catList.Add(cat);
+            }
+            return catList;
+        }
+
+        public async Task<CategoryDto> Updatesync(CategoryDto categoryUpdate)
+        {
+            var getCat = await _category.GetAsync(x => x.Id == categoryUpdate.Id);
+            Category category = new Category
+            {
+                CreateDate = getCat.CreateDate,
+                CreateUser = getCat.CreateUser,
+               Code=categoryUpdate.Code,
+               Description=categoryUpdate.Description,
+               Id=categoryUpdate.Id,
+               Name=categoryUpdate.Name,
+               UpdateDate=DateTime.Now,
+               UpdateUser=1
+
+            };
+            var updateCat = await _category.UpdateAsync(category);
+            CategoryDto catDto = new CategoryDto
+            {
+                Id = updateCat.Id,
+              Code=updateCat.Code,
+              Name=updateCat.Name,
+              Description=updateCat.Description
+             
+
+            };
+            return catDto;
+        }
+    }
+}
