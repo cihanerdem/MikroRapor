714912af61bbdcf9d703fd3ecf04331ca950fa31
Cihan Erdem <erdem_cihan@hotmail.com>
2022-04-04 14:46:06 +0300

    Apı için gerekli kontroller eklendi

---
+++ WepAPI/Controllers/CategorysController.cs
@@ -0,0 +1,93 @@
+﻿using Business.Abstract;
+using Entities.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WepAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategorysController : ControllerBase
+    {
+        private readonly ICategoryService _category;
+
+        public CategorysController(ICategoryService category)
+        {
+            _category = category;
+        }
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> GetList()
+        {
+            var res = await _category.GetListAsync();
+            if (res != null)
+            {
+                return Ok(res);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpGet]
+        [Route("[action]/{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var res = await _category.GetByIdAsync(id);
+            if (res != null)
+            {
+                return Ok(res);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> Add([FromBody] CategoryDto addCat)
+        {
+            var res = await _category.AddAsync(addCat);
+            if (res != null)
+            {
+                return Ok(res);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPut]
+        [Route("[action]")]
+        public async Task<IActionResult> Update([FromBody] CategoryDto updateCat)
+        {
+            var res = await _category.Updatesync(updateCat);
+            if (res != null)
+            {
+                return Ok(res);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpDelete]
+        [Route("[action]/{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var res = await _category.DeleteAsync(id);
+            if (res)
+            {
+                return Ok(res);
+            }
+            else
+            {
+                return BadRequest(false);
+            }
+        }
+    }
+}
---
+++ WepAPI/Controllers/MenusController.cs
@@ -0,0 +1,93 @@
+﻿using Business.Abstract;
+using Entities.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WepAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenusController : ControllerBase
+    {
+        private readonly IAppMenuService _app;
+
+        public MenusController(IAppMenuService app)
+        {
+            _app = app;
+        }
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> GetList()
+        {
+            var res = await _app.GetListAsync();
+            if (res != null)
+            {
+                return Ok(res);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpGet]
+        [Route("[action]/{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var res = await _app.GetByIdAsync(id);
+            if (res != null)
+            {
+                return Ok(res);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> Add([FromBody] AppMenuDto addMenu)
+        {
+            var res = await _app.AddAsync(addMenu);
+            if (res != null)
+            {
+                return Ok(res);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPut]
+        [Route("[action]")]
+        public async Task<IActionResult> Update([FromBody] AppMenuDto updateMenu)
+        {
+            var res = await _app.Updatesync(updateMenu);
+            if (res != null)
+            {
+                return Ok(res);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpDelete]
+        [Route("[action]/{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var res = await _app.DeleteAsync(id);
+            if (res)
+            {
+                return Ok(res);
+            }
+            else
+            {
+                return BadRequest(false);
+            }
+        }
+    }
+}
