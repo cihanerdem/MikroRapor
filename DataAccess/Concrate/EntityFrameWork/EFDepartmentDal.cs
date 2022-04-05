67c1302e0dbb58c89d85c1dd6e867451ce43c521
Cihan Erdem <erdem_cihan@hotmail.com>
2022-04-05 09:50:22 +0300

    Departman Tablosu için Data Acccess layer oluşturuldu

---
+++ DataAccess/Concrate/EntityFrameWork/EFDepartmentDal.cs
@@ -0,0 +1,10 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrate.Context;
+using Entities.Concrate.BaseEntities;
+
+namespace DataAccess.Concrate.EntityFrameWork
+{
+    public  class EFDepartmentDal:EFBaseRepository<Department,MikroRaporContext>,IDepartmentDal
+    {
+    }
+}
