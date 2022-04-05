b5be7e6cb51431e3742f3a38e88254ea0801ebaa
Cihan Erdem <erdem_cihan@hotmail.com>
2022-04-05 09:49:23 +0300

    Departman Tablosu için Interface oluşturuldu

---
+++ Business/Abstract/IDepartmentService.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IDepartmentService
+    {
+        Task<IEnumerable<DepartmentDto>> GetListAsync();
+        Task<DepartmentDto> GetByIdAsync(int id);
+        // Task<UserDto> GetAsync(Expression<Func<User, bool>> filter);
+        Task<DepartmentDto> AddAsync(DepartmentDto departmentAdd);
+        Task<DepartmentDto> Updatesync(DepartmentDto deparmentUpdate);
+        Task<bool> DeleteAsync(int id);
+    }
+}
