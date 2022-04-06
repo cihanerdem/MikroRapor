4c0d2293aafc6fc8fae3fbdff4df40af232e3aa1
Cihan Erdem <erdem_cihan@hotmail.com>
2022-04-06 16:18:10 +0300

    ReportSource Tablosu için Interface yazıldı

---
+++ Business/Abstract/IReportService.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+   public  interface IReportService
+    {
+        Task<IEnumerable<ReportDetailDto>> GetListAsync();
+        Task<ReportDto> GetByIdAsync(int id);
+        // Task<UserDto> GetAsync(Expression<Func<User, bool>> filter);
+        Task<ReportDto> AddAsync(ReportDto reportAdd);
+        Task<ReportDto> Updatesync(ReportDto reportUpdate);
+        Task<bool> DeleteAsync(int id);
+    }
+}
